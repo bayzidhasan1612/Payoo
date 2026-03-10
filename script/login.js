ce3735bf17d55253f996d1663b1d7e18d57008a8
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-03-10 15:28:21 +0600

    complete the login page

--- script/login.js
+++ script/login.js
@@ -4,9 +4,23 @@ document.getElementById('login-btn').addEventListener('click', function(){
     // 1- get the mobile number
     const numberInput = document.getElementById('input-number');
     const contactNumber= numberInput.value;
-    console.log(contactNumber)
+    console.log(contactNumber);
     // 2- get the pin 
+    const pinInput= document.getElementById('input-pin');
+    const pin = pinInput.value;
+    console.log(pin);
     // 3- match the mobile and pin number 
-    // 3.1- true:::>>> alert>homepage
-    // 3.2- true:::>>> alert>return
+    if(contactNumber== "01234567890" && pin=="1234"){
+
+        // 3.1- true:::>>> alert>homepage
+        alert("login Success")
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    }
+    else{
+
+        // 3.2- true:::>>> alert>return
+        alert("login Failed");
+        return; 
+    }
 })
