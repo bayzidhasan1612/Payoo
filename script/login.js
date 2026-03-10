f04fcc5336798b05d62c487ad9af630198641904
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-03-10 15:03:14 +0600

    my first commit

---
+++ script/login.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById('login-btn').addEventListener('click', function(){
+    // 1- get the mobile number
+    const numberInput = document.getElementById('input-number');
+    const contactNumber= numberInput.value;
+    console.log(contactNumber)
+    // 2- get the pin 
+    // 3- match the mobile and pin number 
+    // 3.1- true:::>>> alert>homepage
+    // 3.2- true:::>>> alert>return
+})
